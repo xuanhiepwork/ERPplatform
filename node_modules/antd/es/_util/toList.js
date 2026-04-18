5888db13da850e4dff6b7740565e8d43219ccdbd
xuanhiep <xuanhiep.work@gmail.com>
2026-04-18 20:34:05 +0700

    style: cleanup main branch to keep only README and gitignore

--- node_modules/antd/es/_util/toList.js
+++
@@ -1,8 +0,0 @@
-import isNonNullable from './isNonNullable';
-const toList = (val, config = {}) => {
-  if (!isNonNullable(val) && config?.skipEmpty) {
-    return [];
-  }
-  return Array.isArray(val) ? val : [val];
-};
-export default toList;
